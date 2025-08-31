39813ababcc27e036ef4968fea3a2d94ee6f025c
Eng-HEMA <hshaabrahym36@gmail.com>
2025-08-31 11:45:18 +0300

    making the form

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,28 +1,19 @@
-import React from 'react'
-import { useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const Form = () => {
-    const userRef = useRef(null)
-    const emailRef = useRef(null)
-    const messageRef = useRef(null)
-    const [user, setUser] = useState()
-    
+  const { register, handleSubmit } = useForm()
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(userRef.current.value)
-        console.log(emailRef.current.value)
-        console.log(messageRef.current.value)
-    }
+  const onSubmit = (data) => {
+    console.log(data)
+  }
 
   return (
-    <form className='flex flex-col gap-2 justify-center items-center h-screen' onSubmit={handleSubmit}>
-      <label htmlFor="name">Name {user}</label>
-      <input type="text" id="name" name="name"  ref={userRef} required onChange={(e) => setUser(e.target.value)}/>
+    <form className='flex flex-col gap-2 justify-center items-center h-screen' onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" {...register('name')} />
       <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" ref={emailRef} required />
-      <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" ref={messageRef}></textarea>
+      <input type="email" id="email" name="email" {...register('email')} />
+    
       <button type="submit" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Send</button>
     </form>
   )
