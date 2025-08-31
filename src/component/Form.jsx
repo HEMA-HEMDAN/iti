9127b4565331a80c1b3c6c42fac7a617cdf5fe6c
Eng-HEMA <hshaabrahym36@gmail.com>
2025-08-31 11:46:28 +0300

    some thing'

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,7 +1,8 @@
 import { useForm } from 'react-hook-form'
 
 const Form = () => {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  console.log(errors);
 
   const onSubmit = (data) => {
     console.log(data)
@@ -9,11 +10,11 @@ const Form = () => {
 
   return (
     <form className='flex flex-col gap-2 justify-center items-center h-screen' onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" name="name" {...register('name')} />
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" {...register('email')} />
-    
+      <label htmlFor="name">Name {errors.name && <span className='text-red-500'>{errors.name.message}</span>} </label>
+      <input type="text" id="name" name="name" {...register('name', { required: 'Name is required' })} />
+      <label htmlFor="password">Password {errors.password && <span className='text-red-500'>{errors.password.message}</span>}</label>
+      <input type="password" id="password" name="password" {...register('password', { required: 'Password is required', minLength: { value: 6, message: 'Password must be at least 6 characters' }})} />
+
       <button type="submit" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Send</button>
     </form>
   )
